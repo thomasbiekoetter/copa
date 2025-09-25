4feba725e75e965fde503f4423538e096010c7a5
Thomas <thomas.biekoetter@desy.de>
2025-09-25 20:34:01 +0200

    Started module to collect standard prior functions.

---
+++ src/prior_functions.f90
@@ -0,0 +1,84 @@
+module copa__prior_functions
+
+  use copa__config, only : wp
+
+  implicit none
+
+  private
+
+  real(wp), parameter :: huge_negative = -1.0e45_wp
+  real(wp), parameter :: gaussian_f = 1.0e0_wp
+  real(wp), parameter :: gaussian_eps = 1.0e-10_wp
+
+  public :: uniform_prior
+  public :: log_uniform_prior
+  public :: gaussian_prior
+
+contains
+
+  subroutine uniform_prior(theta, lower, upper, logp)
+
+    real(wp), intent(in) :: theta(:)
+    real(wp), intent(in) :: lower(:)
+    real(wp), intent(in) :: upper(:)
+    real(wp), intent(out) :: logp
+
+    if (all(theta >= lower) .and. all(theta < upper)) then
+      logp = 0.0e0_wp
+    else
+      logp = huge_negative
+    end if
+
+  end subroutine uniform_prior
+
+  subroutine log_uniform_prior()
+
+  end subroutine log_uniform_prior
+
+  subroutine gaussian_prior(theta, mu, sigma, f_sigma, logp)
+
+    real(wp), intent(in) :: theta(:)
+    real(wp), intent(in) :: mu(:)
+    real(wp), intent(in), optional :: sigma(:)
+    real(wp), intent(in), optional :: f_sigma(:)
+    real(wp), intent(out) :: logp
+
+    real(wp), allocatable :: sig(:)
+    real(wp), allocatable :: f(:)
+
+    allocate(sig(size(theta)))
+    allocate(f(size(theta)))
+
+    if (present(sigma)) then
+      sig = sigma
+    else
+      if (present(f_sigma)) then
+        f = f_sigma
+      else
+        f = gaussian_f
+      end if
+      sig = max(f * abs(mu), gaussian_eps)
+    end if
+
+    logp = -0.5_wp * sum(((theta - mu) / sig) ** 2)
+
+  end subroutine gaussian_prior
+
+    ! Log-uniform prior (Jeffrey-like)
+!   if (any(theta_lower <= 0.0e0_wp)) then
+!     write(*,*) "Lower limit of thetas must be positive for log-uniform prior."
+!     call exit
+!   end if
+!   logp = hugeneg
+!   if (all((theta >= theta_lower) .and. (theta <= theta_upper))) then
+!     logp = 0.0e0_wp
+!     do i = 1, size(theta)
+!       ! Metropolis–Hastings acceptance rule -> ln (not log10)
+!       logp = logp - log(theta(i)) - (log(log(theta_upper(i)) -  &
+!         log(theta_lower(i))))
+!     end do
+!   end if
+
+
+
+end module copa__prior_functions
